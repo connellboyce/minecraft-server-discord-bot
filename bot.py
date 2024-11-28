50c319c2886e6cf4518dd590beb44291fea167c3
Connell Boyce <56594465+connellboyce@users.noreply.github.com>
2024-11-28 14:19:51 -0500

    feat: Add Stop and Status commands

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 BASE_URL = os.getenv('BASE_URL')
 START_ENDPOINT = BASE_URL + "/start"
+STOP_ENDPOINT = BASE_URL + "/stop"
+STATUS_ENDPOINT = BASE_URL + "/manage-state"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -28,11 +30,44 @@ async def mcstart(ctx):
         if response.status_code == 200:
             await ctx.send(response.text)
         else:
-            await ctx.send("Failed to reach the server manager job. If this continues, notify an admin.")
+            await ctx.send(
+                "Failed to reach the server manager job. If this continues, notify an admin."
+            )
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
 
-keepAlive()
+
+@bot.command()
+async def mcstop(ctx):
+    # Make an API request
+    try:
+        response = requests.post(STOP_ENDPOINT)
+        if response.status_code == 200:
+            await ctx.send(response.text)
+        else:
+            await ctx.send(
+                "Failed to reach the server manager job. If this continues, notify an admin."
+            )
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+
+
+@bot.command()
+async def mcstatus(ctx):
+    # Make an API request
+    try:
+        response = requests.post(STATUS_ENDPOINT)
+        if response.status_code == 200:
+            await ctx.send(response.text)
+        else:
+            await ctx.send(
+                "Failed to reach the server manager job. If this continues, notify an admin."
+            )
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+
+
+# keepAlive()
 
 # Run the bot
 bot.run(BOT_TOKEN)
