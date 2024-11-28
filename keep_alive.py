5cee10e566094ff3cd3e8e2bcf5fe346b7e3f01f
Connell Boyce <56594465+connellboyce@users.noreply.github.com>
2024-11-28 14:21:30 -0500

    chore: Uncomment keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,20 +1,20 @@
-# from flask import Flask
-# from threading import Thread
+from flask import Flask
+from threading import Thread
 
-# # This web server exists to keep bot running full-time
+# This web server exists to keep bot running full-time
 
-# app = Flask('')
-
-
-# @app.route('/')
-# def home():
-# 	return "Running..."
+app = Flask('')
 
 
-# def run():
-# 	app.run(host='0.0.0.0', port=8080)
+@app.route('/')
+def home():
+	return "Running..."
 
 
-# def keepAlive():
-# 	t = Thread(target=run)
-# 	t.start()
+def run():
+	app.run(host='0.0.0.0', port=8080)
+
+
+def keepAlive():
+	t = Thread(target=run)
+	t.start()
